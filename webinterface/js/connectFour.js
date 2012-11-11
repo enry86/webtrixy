8c7f186b71cee6a6900585d73de6b5c56f9e5213
Enrico Sartori <sartori.enrico@gmail.com>
2012-11-11 22:05:06 +0100

    Victory conditions working, interface now supports human vs human play

--- webinterface/js/connectFour.js
+++ webinterface/js/connectFour.js
@@ -7,17 +7,23 @@ var HEADER = 50;
 var COLS = 7;
 var ROWS = 6;
 
+var WINCOND = 4;
+
 var boardElement = null;
+var resultElement = null;
 var drawContext = null;
 var boardModel = null;
 
 var colors = ["FD0", "F30"];
 var currentPlayer = 1;
+var gamePlaying = true;
 
-function initBoard (boardName) {
-    var ele = document.getElementById (boardName);
-    if (ele != null) {
-        boardElement = ele;
+function initBoard (boardName, resultName) {
+    var board = document.getElementById (boardName);
+    var result = document.getElementById (resultName);
+    if (board != null && result != null) {
+        resultElement = result;
+        boardElement = board;
         boardElement.width = WIDTH;
         boardElement.height = HEIGHT + HEADER;
         drawContext = boardElement.getContext ("2d");
@@ -80,7 +86,7 @@ function hintMove (e) {
 }
 
 function isMoveLegal (move) {
-    return boardModel[0][move] == 0;
+    return (gamePlaying && boardModel[0][move] == 0);
 }
 
 function resetMoveHint () {
@@ -137,6 +143,7 @@ function getMove (pos)  {
 }
 
 function initBoardModel () {
+    gamePlaying = true;
     boardModel = new Array ();
     for (var r = 0; r < ROWS; r++) {
         boardModel[r] = new Array();
@@ -154,6 +161,8 @@ function performMove (e) {
         currentPlayer = (currentPlayer % 2) + 1;
         drawBoard ();
         drawModel ();
+        var win = checkWinner();
+        if (win > 0) endGame (win);
     }
 }
 
@@ -169,4 +178,165 @@ function storeMove (move) {
         }
     }
     return found;
+}
+
+function checkWinner () {
+    var win = checkHoriz ();
+    if (win == 0) win = checkVert ();
+    if (win == 0) win = checkDiagLeft ();
+    if (win == 0) win = checkDiagRight ();
+    return win;
+}
+
+function checkHoriz () {
+    var win = 0;
+    var found = false;
+
+    for (var r = 0; r < ROWS && !found; r++) {
+        var currWin = 0;
+        var count = 0;
+        for (var c = 0; c < COLS && !found; c++) {
+            var val = boardModel[r][c];
+            if (val == currWin && val > 0) {
+                count++;
+                if (count == WINCOND) found = true;
+            }
+            else {
+                currWin = val;
+                count = 1;
+            }
+        }
+    }
+    if (count == WINCOND && currWin != 0) win = currWin;
+    return win;
+}
+
+function checkVert () {
+    var win = 0;
+    var found = false;
+    var currWin = 0;
+    var count = 0;
+    for (var c = 0; c < COLS && !found; c++) {
+        currWin = 0;
+        count = 0;
+        for (var r = 0; r < ROWS && !found; r++) {
+            var val = boardModel[r][c];
+            if (val == currWin && val > 0) {
+                count++;
+                if (count == WINCOND) found = true;
+            }
+            else {
+                currWin = val;
+                count = 1;
+            }
+        }
+    }
+    if (count == WINCOND && currWin != 0) win = currWin;
+    return win;
+}
+
+function checkDiagLeft () {
+    var win = 0;
+    var found = false;
+    var currWin = 0;
+    var currWin2 = 0;
+    var count = 0;
+    var count2 = 0;
+
+    var shift = 0;
+
+    for (var startRow = ROWS - 1; startRow >= WINCOND - 1 && !found; startRow--) {
+        var c = 0;
+        var r = startRow;
+        currWin = 0;
+        count = 0;
+        currWin2 = 0;
+        count2 = 0;
+
+        while (r >= 0 && !found) {
+            var val = boardModel[r][c];
+            if (val == currWin && val > 0) {
+                count++;
+                if (count == WINCOND) found = true;
+            }
+            else {
+                currWin = val;
+                count = 1;
+            }
+
+            val = boardModel[r + shift][c + shift + 1];
+            if (val == currWin2 && val > 0) {
+                count2++;
+                if (count2 == WINCOND) {
+                    found = true;
+                    currWin = currWin2;
+                }
+            }
+            else {
+                currWin2 = val;
+                count2 = 1;
+            }
+            r--;
+            c++;
+        }
+        shift++;
+    }
+    if (found) win = currWin;
+    return win;
+}
+
+function checkDiagRight () {
+    var win = 0;
+    var found = false;
+    var currWin = 0;
+    var currWin2 = 0;
+    var count = 0;
+    var count2 = 0;
+
+    var shift = 0;
+
+    for (var startRow = ROWS - 1; startRow >= WINCOND - 1 && !found; startRow--) {
+        var c = COLS - 1;
+        var r = startRow;
+        currWin = 0;
+        count = 0;
+        currWin2 = 0;
+        count2 = 0;
+
+        while (r >= 0 && !found) {
+            var val = boardModel[r][c];
+            if (val == currWin && val > 0) {
+                count++;
+                if (count == WINCOND) found = true;
+            }
+            else {
+                currWin = val;
+                count = 1;
+            }
+
+            val = boardModel[r + shift][c - (shift + 1)];
+            if (val == currWin2 && val > 0) {
+                count2++;
+                if (count2 == WINCOND) {
+                    found = true;
+                    currWin = currWin2;
+                }
+            }
+            else {
+                currWin2 = val;
+                count2 = 1;
+            }
+            r--;
+            c--;
+        }
+        shift++;
+    }
+    if (found) win = currWin;
+    return win;
+}
+
+function endGame (win) {
+    gamePlaying = false;
+    var str = "Player " + win + " won the game!";
+    resultElement.innerHTML = str;
 }
